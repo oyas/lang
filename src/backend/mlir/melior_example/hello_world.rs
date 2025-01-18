5197c67002e94bc975928d4be27c8f0e86997797
oyas <poteto83@gmail.com>
2025-01-19 05:41:49 +0900

    Add ExecutionEngine of MLIR. Emit llvm ir.

--- src/backend/mlir/melior_example/hello_world.rs
+++ src/backend/mlir/melior_example/hello_world.rs
@@ -1,9 +1,18 @@
+use std::ffi::CStr;
+
+use inkwell::llvm_sys;
 use melior::{
-    Context,
-    dialect::{arith, DialectRegistry, func},
-    ir::{*, attribute::{StringAttribute, TypeAttribute}, r#type::FunctionType},
+    dialect::{arith, func, DialectRegistry},
+    ir::{
+        attribute::{StringAttribute, TypeAttribute},
+        r#type::FunctionType,
+        *,
+    },
+    pass,
     utility::register_all_dialects,
+    Context, ExecutionEngine,
 };
+use mlir_sys::{mlirTranslateModuleToLLVMIR, LLVMOpaqueContext};
 
 pub fn hello() {
     let registry = DialectRegistry::new();
@@ -14,35 +23,93 @@ pub fn hello() {
     context.load_all_available_dialects();
 
     let location = Location::unknown(&context);
-    let module = Module::new(location);
+    let mut module = Module::new(location);
 
     let index_type = Type::index(&context);
 
     module.body().append_operation(func::func(
         &context,
         StringAttribute::new(&context, "add"),
-        TypeAttribute::new(FunctionType::new(&context, &[index_type, index_type], &[index_type]).into()),
+        TypeAttribute::new(
+            FunctionType::new(&context, &[index_type, index_type], &[index_type]).into(),
+        ),
         {
             let block = Block::new(&[(index_type, location), (index_type, location)]);
 
             let sum = block.append_operation(arith::addi(
                 block.argument(0).unwrap().into(),
                 block.argument(1).unwrap().into(),
-                location
+                location,
             ));
 
-            block.append_operation(func::r#return( &[sum.result(0).unwrap().into()], location));
+            block.append_operation(func::r#return(&[sum.result(0).unwrap().into()], location));
 
             let region = Region::new();
             region.append_block(block);
             region
         },
-        &[],
+        &[
+            (
+                Identifier::new(&context, "llvm.emit_c_interface"),
+                Attribute::unit(&context),
+            ), // required to execute invoke_packed
+        ],
         location,
     ));
 
     assert!(module.as_operation().verify());
-    println!("{:?}", module.as_operation());
+    println!("{}", module.as_operation());
+
+    // pass
+    let pass_manager = pass::PassManager::new(&context);
+    pass_manager.add_pass(pass::conversion::create_to_llvm());
+    assert_eq!(pass_manager.run(&mut module), Ok(()));
+    println!("{}", module.as_operation());
+
+    // execute
+    let engine = ExecutionEngine::new(&module, 0, &[], true);
+    let mut argument1: usize = 42;
+    let mut argument2: usize = 43;
+    let mut result: usize = 0;
+    let res = unsafe {
+        engine.invoke_packed(
+            "add",
+            &mut [
+                &mut argument1 as *mut usize as *mut (),
+                &mut argument2 as *mut usize as *mut (),
+                &mut result as *mut usize as *mut (),
+            ],
+        )
+    };
+    assert_eq!(res, Ok(()));
+    println!("result = {}", result);
+
+    // emit llvm ir (inkwell)
+    // let llvm_context = inkwell::context::Context::create();
+    // let llvm_module = unsafe {
+    //     let m = mlirTranslateModuleToLLVMIR(module.as_operation().to_raw(), llvm_context.raw() as *mut LLVMOpaqueContext);
+    //     inkwell::module::Module::new(m as *mut inkwell::llvm_sys::LLVMModule)
+    // };
+    // println!("----- Generated LLVM IR -----");
+    // println!("{}", llvm_module.to_string());
+    // println!("----- End of LLVM IR -----");
+
+    // emit llvm ir (llvm-sys)
+    let llvm_module = unsafe {
+        let llvm_context = llvm_sys::core::LLVMContextCreate();
+        let llvm_module = mlirTranslateModuleToLLVMIR(
+            module.as_operation().to_raw(),
+            llvm_context as *mut LLVMOpaqueContext,
+        );
+        let ir_str = llvm_sys::core::LLVMPrintModuleToString(llvm_module as *mut llvm_sys::LLVMModule);
+        CStr::from_ptr(ir_str).to_str().unwrap()
+    };
+    println!("----- Generated LLVM IR -----");
+    println!("{}", llvm_module);
+    println!("----- End of LLVM IR -----");
+
+    // emit obj
+    engine.dump_to_object_file("test.o");
 }
 
 #[cfg(test)]
